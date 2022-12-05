7524310c3feb7e360088554aa8da61199c82f7b3
kylem <kyle.mcquillan.01@gmail.com>
2022-12-05 00:09:42 +0000

    Added Remove Function (Not Working) :construction:

--- ADS_CA2_Tree/BinaryTree.h
+++ ADS_CA2_Tree/BinaryTree.h
@@ -1,14 +1,139 @@
 #pragma once
+#include "TNode.h"
 //if E == Student (email, dob, address, id,...)
 //then K = Key(email, dob, address)
 
 template <typename K, typename E>
 class BinaryTree
 {
+	void addItemToArray(TNode<K,E>* node, int& pos, int* arr);
+
+
 public:
-    void add(K,E item);
+
+
+	TNode<K,E> *root;
+	BinaryTree();
+    void add(K keyItem, E item);
+    bool remove(K keyItem, E item);
+
+	void clear();
+
+	int count();
+
+
 
 	E* search(K key) {
 		return nullptr;
 	}
+	
 };
+template <typename K, typename E>
+BinaryTree<K,E>::BinaryTree()
+{
+	root = nullptr;
+}
+template <typename K, typename E>
+void BinaryTree<K,E>::add(K keyItem,E item)
+{
+    if (root == nullptr)
+    {
+        root = new TNode<K,E>();
+        root->setItem(item,keyItem);
+    }
+    else
+    {
+        root->add(item,keyItem);
+    }
+}
+
+template <typename K, typename E>
+bool BinaryTree<K,E>::remove(K keyItem, E item)
+{
+	TNode<K,E>* toBeRemoved = root;
+	TNode<K,E>* parent = nullptr;
+	bool found = false;
+
+	while (!found && toBeRemoved != nullptr)
+	{
+
+		if (toBeRemoved->getItem() == item && toBeRemoved->getKey()== keyItem)
+		{
+
+			found = true;
+		}
+		else
+		{
+			parent = toBeRemoved;
+			if (toBeRemoved->getItem() > item && toBeRemoved->getKey() > keyItem)
+			{
+				toBeRemoved = toBeRemoved->getLeft();
+			}
+			else
+			{
+				toBeRemoved = toBeRemoved->getRight();
+			}
+		}
+	}
+	if (!found)
+		return false;
+
+	if (toBeRemoved->getLeft() == nullptr || toBeRemoved->getRight() == nullptr)
+	{
+		TNode<K,E>* newChild;
+		if (toBeRemoved->getLeft() == nullptr)
+		{
+			newChild = toBeRemoved->getRight();
+		}
+		else
+		{
+			newChild = toBeRemoved->getLeft();
+		}
+		if (parent == nullptr)
+		{
+			root = newChild;
+		}
+		else if (parent->getLeft() == toBeRemoved)
+		{
+			parent->setLeft(newChild);
+		}
+		else
+		{
+			parent->setRight(newChild);
+		}
+		return true;
+	}
+
+	TNode<K,E>* smallestParent = toBeRemoved;
+	TNode<K,E>* smallest = toBeRemoved->getRight();
+	while (smallest->getLeft() != nullptr)
+	{
+		smallestParent = smallest;
+		smallest = smallest->getLeft();
+	}
+	toBeRemoved->setItem(smallest->getItem());
+	if (smallestParent == toBeRemoved)
+	{
+		smallestParent->setRight(smallest->getRight());
+	}
+	else
+	{
+		smallestParent->setLeft(smallest->getRight());
+	}
+
+}
+
+template <typename K,typename E>
+void BinaryTree<K,E>::clear()
+{
+	delete root;
+	root = nullptr;
+}
+
+template <typename K,typename E>
+int BinaryTree<K,E>::count()
+{
+	if (root == nullptr)
+		return 0;
+	return root->count();
+}
--- BinaryTreeTest/BinaryTreeTest.cpp
+++ BinaryTreeTest/BinaryTreeTest.cpp
@@ -10,9 +10,72 @@ namespace BinaryTreeTest
 	{
 	public:
 		
-		/*TEST_METHOD(TestAddToEmptyTree)
+		TEST_METHOD(TestAddToEmptyTree)
 		{
-		
-		}*/
+			BinaryTree<int,int> tree;
+			tree.add(1,2);
+			Assert::IsNotNull(tree.root);
+			Assert::AreEqual(1, tree.root->getItem());
+		}
+
+		TEST_METHOD(TestAddToRootLeft)
+		{
+			BinaryTree<int,int> tree;
+			tree.add(2,2);
+			tree.add(1,1);
+			Assert::IsNotNull(tree.root);
+			Assert::AreEqual(2, tree.root->getItem());
+			TNode<int,int>* pLeft = tree.root->getLeft();
+			Assert::IsNotNull(pLeft);
+			Assert::AreEqual(1, pLeft->getItem());
+		}
+		TEST_METHOD(TestAddToRootRight)
+		{
+			BinaryTree<int,int> tree;
+			tree.add(2,2);
+			tree.add(3,3);
+			Assert::IsNotNull(tree.root);
+			Assert::AreEqual(2, tree.root->getItem());
+			TNode<int,int>* pRight = tree.root->getRight();
+			Assert::IsNotNull(pRight);
+			Assert::AreEqual(3, pRight->getItem());
+		}
+		TEST_METHOD(TestCountOnFullTree)
+		{
+			BinaryTree<int,int> tree;
+			tree.add(4,4);
+			tree.add(2,2);
+			tree.add(6,6);
+			tree.add(1,1);
+			tree.add(3,3);
+			tree.add(5,5);
+			tree.add(7,7);
+
+			Assert::AreEqual(7, tree.count());
+		}
+
+		TEST_METHOD(TestRemoveItemNotPresent)
+		{
+			BinaryTree<int,int> tree;
+			tree.add(4,4);
+			tree.add(2,2);
+			tree.add(6,6);
+			tree.add(1,1);
+			tree.add(3,3);
+			tree.add(5,5);
+			tree.add(7,7);
+			Assert::AreEqual(7, tree.count());
+			tree.remove(8,8);
+			Assert::AreEqual(7, tree.count());
+			Assert::AreEqual(4, tree.root->getItem());
+			Assert::AreEqual(2, tree.root->getLeft()->getItem());
+			Assert::AreEqual(6, tree.root->getRight()->getItem());
+			TNode<int,int>* left = tree.root->getLeft();
+			TNode<int,int>* right = tree.root->getRight();
+			Assert::AreEqual(1, left->getLeft()->getItem());
+			Assert::AreEqual(3, left->getRight()->getItem());
+			Assert::AreEqual(5, right->getLeft()->getItem());
+			Assert::AreEqual(7, right->getRight()->getItem());
+		}
 	};
 }
